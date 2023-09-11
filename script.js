b321c83be2129771f5eae7fda84a33f3e1eac186
unknown <barryopeyemi@gmail.com>
2023-09-11 17:50:07 +0100

    zuri

--- script.js
+++ script.js
@@ -9,18 +9,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Function to update the current day of the week and UTC time
     function updateCurrentInfo() {
-        const dayOfTheWeekElement = document.querySelector('[data-testid="currentDayOfTheWeek"]');
-        const utcTimeElement = document.querySelector('[data-testid="currentUTCTime"]');
+        const Week= document.querySelector('[data-testid="currentDayOfTheWeek"]');
+        const utc= document.querySelector('[data-testid="currentUTCTime"]');
 
-        const Week = OurWeek();
-        const currentUTCTime = new Date().getTime();
+        const Weekend = OurWeek();
+        const currentUTCTime = Date.now();
 
-        dayOfTheWeekElement.textContent = `Current Day of the Week: ${Week}`;
-        utcTimeElement.textContent = `Current UTC Time: ${currentUTCTime}`;
+        Week.textContent = `Current Day of the Week: ${Weekend}`;
+        utc.textContent = `Current UTC Time: ${currentUTCTime}`;
     }
 
     // Update the current information every second
     updateCurrentInfo();
-    setInterval(updateCurrentInfo, 1000);
+    setInterval(updateCurrentInfo, 1);
 });
 
